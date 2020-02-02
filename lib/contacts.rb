5997cdc3ed4c209a9fc3267159575d224e38b3b2
james Johnson <2013matthew2013@gmail.com>
2020-02-02 18:20:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,14 +15,13 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry (contacts)
+def remove_strawberry ("contacts")
  contacts.each do |person, contact_details_hash|
   contact_details_hash.each do |attribute, data|
     if attribute == :favorite_icecream_flavors
         data.delete_if{ |ice_cream| ice_cream == "strawberry"}
       end
-    end
- def remove_strawberry(contacts)
+ def remove_strawberry ("contacts")
      freddy = contacts["Freddy Mercury"]
      freddy.each do |attribute, value|
     if attribute == :favorite_ice_cream_flavors
